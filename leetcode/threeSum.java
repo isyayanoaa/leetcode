5d8ffac20ce74db088f55f6f577116437c82ddda
fengweiyi <fwy184373@163.com>
2024-04-08 11:03:58 +0800

    4.8

---
+++ leetcode/threeSum.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class threeSum {
+    public List<List<Integer>> threesum(int[] nums){
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for(int i=0;i<nums.length-2;++i){
+            if (nums[i]>0) break;
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int left = i + 1;
+            int right = nums.length - 1;
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum > 0){
+                    right--;
+                }
+                else if(sum < 0){
+                    left++;
+                }
+                else{
+                    result.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    while(left<right && nums[left]==nums[left+1]) left++;
+                    while(left<right && nums[right]==nums[right-1]) right--;
+                    left++;
+                    right--;
+
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1,0,1,2,-1,-4};
+        threeSum solution = new threeSum();
+        List<List<Integer>> res = solution.threesum(nums);
+        for (List<Integer> list : res) {
+            for (Integer num : list) {
+                System.out.print(num + " ");
+            }
+            System.out.println();  // 换行，以分隔不同的内部列表
+        }
+    }
+}
